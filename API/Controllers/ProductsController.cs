c86bd8516f7fd6e78357b66b9845b4cd10193984
Yuriy Lyukov <lyukov.yura@gmail.com>
2021-02-04 17:27:24 +0200

    feat: services and middlewares moved to another folder

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -37,7 +37,7 @@ namespace API.Controllers
             var spec = new ProductsWithTypesAndBrandsSpecification();
             
             var products = await _productsRepo.ListAsync(spec);
-
+           
             return Ok(_mapper
                 .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
         }
