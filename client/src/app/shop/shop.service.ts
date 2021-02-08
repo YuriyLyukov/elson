0f0e075a10f58f697c088406d1c1952ae74f976b
Yuriy Lyukov <lyukov.yura@gmail.com>
2021-02-08 15:11:31 +0200

    feat: added new components, added sort, filter functionality

---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
+import {IPagination} from '../shared/models/pagination';
+import {Observable} from 'rxjs';
+import {IBrand} from '../shared/models/brand';
+import {IType} from '../shared/models/productType';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+  getProducts(brandId?: number, typeId?: number, sort?: string): Observable<IPagination>
+  {
+    let params = new HttpParams();
+    if (brandId){
+      params = params.append('brandId', brandId.toString());
+    }
+    if (typeId){
+      params = params.append('typeId', typeId.toString());
+    }
+    if (sort){
+      params = params.append( 'sort', sort);
+    }
+    return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response' , params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
+  }
+  getBrands(): Observable<IBrand[]>{
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+  getTypes(): Observable<IType[]>{
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+  }
+}
