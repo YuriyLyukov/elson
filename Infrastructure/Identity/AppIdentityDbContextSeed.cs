16002bda26d989089d61cc766fd58e3bd06681f3
Yuriy Lyukov <lyukov.yura@gmail.com>
2021-02-15 14:24:20 +0200

    feat: added JWT token login and registration func,added new Entities, DTO, MappingProfiles, services

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUserAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser()
+                {
+                    DisplayName = "Bob",
+                    Email = "bob@test.com",
+                    UserName = "bob@test.com",
+                    Address = new Address()
+                    {
+                        FirstName = "Bob",
+                        LastName = "Bobity",
+                        Street = "10 The Streer",
+                        City = "NewYork",
+                        State = "NY",
+                        ZipCode = "90201"
+                    }
+                };
+
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+    }
+}
